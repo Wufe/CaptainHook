30542cb63b4575855ca292a326c6327475d1fdd9
Wufe <wufe92@gmail.com>
2016-10-11 01:54:36 +0200

    Added validateMethod test

--- test/core/server/index.js
+++ test/core/server/index.js
@@ -1,18 +1,30 @@
 var assert = require( 'assert' );
-var CHook = require( '../../../build/lib/chook.js' );
+var expect = require( 'chai' ).expect;
+var Server = require( '../../../build/lib/chook.js' ).Server;
 
-describe( 'CHook.Server', function(){
-	it( 'should be a function', function(){
-		assert.equal( typeof CHook.Server, 'function' );
-	});
+describe( 'Server', function(){
+	
 });
 
-describe( 'CHook.Server', function(){
+describe( 'Server', function(){
 	var server;
 	before( function(){
-		server = new CHook.Server();
+		server = new Server();
+	});
+	it( 'should be a function', function(){
+		assert.equal( typeof Server, 'function' );
 	});
 	it( 'should create a express app', function(){
 		assert.equal( typeof server, 'object' );
+	});
+});
+
+describe( 'Server.validateMethod', function(){
+	var server;
+	before( function(){
+		server = new Server();
+	});
+	it( 'should throw a TypeError with invalid method', function(){
+		expect( server.validateMethod.bind( server, 'asd' ) ).to.throw( 'Not a valid method.' );
 	});
 });
