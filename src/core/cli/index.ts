6a01f60970a526f7c138a87ec115fefc646e2e28
Giuseppe Pagano <giuseppe.pagano.p@gmail.com>
2016-10-11 21:58:54 +0200

    function to parse cli arguments

--- src/core/cli/index.ts
+++ src/core/cli/index.ts
@@ -1,8 +1,26 @@
 import * as Yargs from 'yargs';
 
 export default class Cli{
+    args: any[];
+    opts: any;
+    command: string;
+
 	constructor(){
-        let argv:any = Yargs.argv
-		console.log( argv );
-	}	
+        let argv:any = Yargs.argv;
+		this.parse(argv);
+  	}
+
+    parse = (argv: any): void => {
+        var _args = argv._;
+        this.command = _args[0];
+        this.args = _args.splice(1);
+        this.opts = {};
+
+        for(var key in argv){
+            if(key != "_" && key != "$0"){
+                this.opts[key] = argv[key]
+            }
+        }
+    }
+
 }
