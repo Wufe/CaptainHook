15d327c29b4e66a79348ccc0fd2bf54984fb6f75
Giuseppe Pagano <giuseppe.pagano.p@gmail.com>
2016-10-11 11:42:23 +0200

    refactored tests using should

--- test/core/server/index.js
+++ test/core/server/index.js
@@ -8,10 +8,10 @@ describe( 'Server', function(){
 		server = new Server();
 	});
 	it( 'should be a function', function(){
-		assert.equal( typeof Server, 'function' );
+		Server.should.be.a.Function;
 	});
 	it( 'should create a express app', function(){
-		assert.equal( typeof server, 'object' );
+		server.should.be.an.Object;
 	});
 });
 
@@ -21,13 +21,13 @@ describe( 'Server.validateMethod', function(){
 		server = new Server();
 	});
 	it( 'should throw a TypeError with invalid method', function(){
-		expect( server.validateMethod.bind( server, 'asd' ) ).to.throw( 'Not a valid method.' );
+		server.validateMethod.bind( server, 'asd' ).should.throw( 'Not a valid method.' );
 	});
 	let validMethods = [ 'get', 'post', 'patch', 'put', 'delete', 'all' ];
 	for( let i = 0; i < validMethods.length; i++ ){
 		let method = validMethods[i];
 		it( 'should be fine with a valid ' + method + ' method', function(){
-			expect( server.validateMethod.bind( server, method ) ).to.not.throw( 'Not a valid method.' );
+			server.validateMethod.bind( server, method ).should.not.throw( 'Not a valid method.' );
 		});
 	}
 	
