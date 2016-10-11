c78cb82ecfdcf2d9a110c08aff98d33c11422458
Wufe <wufe92@gmail.com>
2016-10-11 13:00:35 +0200

    Configuration class base

---
+++ src/core/configuration/Configuration.ts
@@ -0,0 +1,11 @@
+const path = require( 'path' );
+
+export default class Configuration{
+
+	configurationPath: string;
+
+	constructor(){
+		this.configurationPath = path.join( __dirname, "..", "..", "chook.config.yml" );
+	}
+
+}
