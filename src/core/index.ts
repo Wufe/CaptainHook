177abc7e1795e9475c23e096eedecf67a2ade5d5
Wufe <wufe92@gmail.com>
2016-10-14 19:45:16 +0200

    Changed index to dev-index in order to figure out which represents the test environment and wich not

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,22 +1,7 @@
 import Cli from './cli';
-import EntryManager from './chook/EntryManager';
-import Server from './server/Server';
 import ServerManager from './server/ServerManager';
-import Configuration from './configuration/Configuration';
-import * as Utils from './chook/Utils';
 
 export {
-	Cli,
-	Configuration,
-	EntryManager,
-	Server,
-	ServerManager,
-	Utils
+	Cli,	
+	ServerManager
 };
-
-/* Sample code */
-const server: Server = new Server();
-server.addRoute( 'get', '/webhook/idaji', (request, response) => {
-	response.send( 'OK' );
-});
-//server.listen();
