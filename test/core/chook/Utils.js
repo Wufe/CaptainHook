0d296b5f1e60bc377f457d857ba1534f220b7679
Wufe <wufe92@gmail.com>
2016-10-14 17:27:57 +0200

    Moved before and after modifier to the start and the end of the tests that require them

--- test/core/chook/Utils.js
+++ test/core/chook/Utils.js
@@ -2,11 +2,13 @@ const fs = require( 'fs' );
 const Utils = require( '../../../build/lib/chook.js' ).Utils;
 
 describe( 'Utils', function(){
+	let testFile = './utils.test';
+	let testDirectory = './utils.test.dir';
+	before( function(){
+		fs.writeFileSync( testFile, '' );
+		fs.mkdirSync( testDirectory );
+	});
 	describe( 'fileExists', function(){
-		let testFile = './utils.test';
-		before( function(){
-			fs.writeFileSync( testFile, '' );
-		});
 		it( 'should be a function', function(){
 			Utils.fileExists.should.be.a.Function;
 		});
@@ -16,17 +18,8 @@ describe( 'Utils', function(){
 		it( 'should return true if a file exists', function(){
 			Utils.fileExists( testFile ).should.be.exactly(true);
 		});
-		after( function(){
-			fs.unlinkSync( testFile );
-		});
 	});
 	describe( 'isFile', function(){
-		let testFile = './utils.test';
-		let testDirectory = './utils.test.dir';
-		before( function(){
-			fs.writeFileSync( testFile, '' );
-			fs.mkdirSync( testDirectory );
-		});
 		it( 'should be a function', function(){
 			Utils.isFile.should.be.a.Function;
 		});
@@ -39,14 +32,14 @@ describe( 'Utils', function(){
 		it( 'should return true if the path is a file', function(){
 			Utils.isFile( testFile ).should.be.exactly(true);
 		});
-		after( function(){
-			fs.unlinkSync( testFile );
-			fs.rmdirSync( testDirectory );
-		});
 	});
 	describe( 'isDirectory', function(){
 		it( 'should be a function', function(){
 			Utils.isDirectory.should.be.a.Function;
 		});
+	});
+	after( function(){
+		fs.unlinkSync( testFile );
+		fs.rmdirSync( testDirectory );
 	});
 });
