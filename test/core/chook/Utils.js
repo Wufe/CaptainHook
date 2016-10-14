cf60fda954ecde5dda52357c33d326d84067f568
Wufe <wufe92@gmail.com>
2016-10-14 17:13:45 +0200

    Added basic tests for the three functions in utils

--- test/core/chook/Utils.js
+++ test/core/chook/Utils.js
@@ -1,2 +1,19 @@
 const fs = require( 'fs' );
 const Utils = require( '../../../build/lib/chook.js' ).Utils;
+describe( 'Utils', function(){
+	describe( 'fileExists', function(){
+		it( 'should be a function', function(){
+			Utils.fileExists.should.be.a.Function;
+		});
+	});
+	describe( 'isFile', function(){
+		it( 'should be a function', function(){
+			Utils.isFile.should.be.a.Function;
+		});
+	});
+	describe( 'isDirectory', function(){
+		it( 'should be a function', function(){
+			Utils.isDirectory.should.be.a.Function;
+		});
+	});
+});
