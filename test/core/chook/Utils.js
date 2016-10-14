ab0e5a31f796db5efa8c206ba9077ee30da3e658
Wufe <wufe92@gmail.com>
2016-10-14 17:14:24 +0200

    Completed tests for fileExists

--- test/core/chook/Utils.js
+++ test/core/chook/Utils.js
@@ -1,9 +1,23 @@
 const fs = require( 'fs' );
 const Utils = require( '../../../build/lib/chook.js' ).Utils;
+
 describe( 'Utils', function(){
 	describe( 'fileExists', function(){
+		let testFile = './utils.test';
+		before( function(){
+			fs.writeFileSync( testFile, '' );
+		});
 		it( 'should be a function', function(){
 			Utils.fileExists.should.be.a.Function;
+		});
+		it( 'should return false if a file does not exist', function(){
+			Utils.fileExists( './UtilsFileExistsTestFile' ).should.be.exactly(false);
+		});
+		it( 'should return true if a file exists', function(){
+			Utils.fileExists( testFile ).should.be.exactly(true);
+		});
+		after( function(){
+			fs.unlinkSync( testFile );
 		});
 	});
 	describe( 'isFile', function(){
