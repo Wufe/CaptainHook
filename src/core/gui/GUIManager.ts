a6ea79377471d5e5ee1f9eb3b3f5b2437d79518e
Wufe <wufe92@gmail.com>
2016-10-14 17:56:51 +0200

    Created base of the guimanager class

--- src/core/gui/GUIManager.ts
+++ src/core/gui/GUIManager.ts
@@ -1,4 +1,16 @@
 import Server from '../server/Server';
+import * as Utils from '../chook/Utils';
+declare let require:any;
+const Url = require( 'url' );
+const Path = require( 'path' );
+
+/**
+ * Here we are going to serve static files with no middleware,
+ * to not require express' deps
+ */ 
+
+const assetsDir: string = "/assets/";
+const assetsPath: string = Path.resolve( Path.join( '..', 'resources', 'assets' ) );
 
 export default class GUIManager{
 
@@ -9,7 +21,24 @@ export default class GUIManager{
 	}
 
 	initialize(): void{
+		this.addAssetsRoutes();
+	}
 
+	addAssetsRoutes(): void{}
+
+	serveAssets( request: any, response: any, next: any ): void{}
+
+	getUrlPathname( request: any ): string{
+		let url: any = Url.parse( request.url, true );
+		let urlPathname: string = url.pathname;
+		return urlPathname;
+	}
+
+	isAssetsDir( pathname: string ): boolean{
+		let startingWithAssetsDir: any = pathname.match( new RegExp( `^${assetsDir}` ) );
+		if( startingWithAssetsDir === null )
+			return false;
+		return true;
 	}
 
 }
