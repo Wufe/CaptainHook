8f2fdbf0540c9cf585fffb5ddfe24f9734bd9c9a
Wufe <wufe92@gmail.com>
2016-10-29 10:45:21 +0200

    Created setNestedValue and getNestedValue functions

--- src/core/chook/Utils.ts
+++ src/core/chook/Utils.ts
@@ -10,22 +10,55 @@ let fileExists = ( fileName: string ) => {
     }
 };
 
+let getNestedValue = ( obj: any, ...keys: string[] ) => {
+    if( keys.length === 1 ){
+        return obj[ keys[0] ];
+    }else{
+        let value: any = obj;
+        let i: number = 0;
+        while( typeof value != 'undefined' && i < keys.length ){
+            value = value[ keys[i++] ];
+        }
+        return value;
+    }
+};
+
 let isFile = ( fileName: string ) => {
     if( fileExists( fileName ) ){
         return fs.lstatSync( fileName ).isFile();
     }
     return false;
-}
+};
 
 let isDirectory = ( fileName: string ) => {
 	if( fileExists( fileName ) ){
 		return !isFile( fileName );
 	}
 	return false;
-}
+};
+
+let setNestedValue = ( obj: any, value: any, ...keys: string[] ) => {
+    let objSubtree: any = obj;
+    let finalKey: string;
+    while( keys.length > 0 ){
+        if( finalKey ){
+            objSubtree = objSubtree[ finalKey ];    
+        }
+        let key: string = keys[0];
+        keys = keys.slice(1);
+        if( objSubtree[ key ] === undefined || typeof objSubtree[ key ] != "object" ){
+            objSubtree[ key ] = {};
+        }
+        finalKey = key;
+    }
+    objSubtree[ finalKey ] = value;
+    return obj;
+};
 
 export {
 	fileExists,
+    getNestedValue,
 	isFile,
-	isDirectory
+	isDirectory,
+    setNestedValue
 };
