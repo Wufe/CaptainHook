1bff2e3894dfdd80bedaf5cd6563dd5f291b93db
Wufe <wufe92@gmail.com>
2016-10-30 16:56:18 +0100

    First test. Checks the definition of a user model

--- test/core/data/database/index.js
+++ test/core/data/database/index.js
@@ -1,3 +1,10 @@
 const CHook = require( '../../../../build/lib/chook.js' );
 const Database = CHook.Database;
 
+describe( 'Database', function(){
+	describe( 'models', function(){
+		it( 'should contain a model called "user"', function(){
+			Database.models.should.have.property( 'user' );
+		});
+	});
+});
