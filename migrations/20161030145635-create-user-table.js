2dbf85324fe3270f43ef379e9b580eb80bd61408
Wufe <wufe92@gmail.com>
2016-10-30 16:54:02 +0100

    Changed migration: it will now use model definitions script

--- migrations/20161030145635-create-user-table.js
+++ migrations/20161030145635-create-user-table.js
@@ -1,26 +1,8 @@
+var models = require( './models.js' );
+
 module.exports = {
 	up: ( queryInterface, Sequelize ) => {
-		queryInterface.createTable( 'users', {
-			id: {
-				type: Sequelize.INTEGER,
-				primaryKey: true,
-				autoIncrement: true
-			},
-			username: {
-				type: Sequelize.STRING,
-				allowNull: false
-			},
-			password: {
-				type: Sequelize.STRING,
-				allowNull: false
-			},
-			created_at: {
-				type: Sequelize.DATE
-			},
-			updated_at: {
-				type: Sequelize.DATE
-			}
-		});
+		queryInterface.createTable( 'users', models.user( Sequelize ));
 	},
 	down: ( queryInterface, Sequelize ) => {
 		queryInterface.dropTable( 'users' );
