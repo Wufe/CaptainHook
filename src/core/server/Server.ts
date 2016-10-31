5ab56fadb5f5d8fbae89bfb3f9575003e428dbd3
Wufe <wufe92@gmail.com>
2016-10-31 12:42:13 +0100

    Removed addRoute, exposing directly the expressServer variable

--- src/core/server/Server.ts
+++ src/core/server/Server.ts
@@ -2,22 +2,10 @@ import * as Express from 'express';
 
 export default class Server{
 
-	expressServer: any;
+	expressServer: Express.Express;
 
 	constructor(){
 		this.expressServer = Express();
-	}
-
-	addRoute( method: string, uri: string, callback: (request:any, response:any, next?: any) => void ): void{
-		this.validateMethod( method );
-		this.expressServer[ method ]( uri, callback );
-	}
-
-	validateMethod( method: string ): boolean{
-		let isValidMethod: boolean = [ 'get', 'post', 'patch', 'put', 'delete', 'all' ].indexOf( method ) != -1;
-		if( !isValidMethod )
-			throw new TypeError( "Not a valid method." );
-		return true;
 	}
 
 	listen( port:number = 3000 ): void{
