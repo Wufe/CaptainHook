5c59ed6cc1d253a91e63b86fe34258fca638efcf
Wufe <wufe92@gmail.com>
2016-10-31 12:43:22 +0100

    Removed validateMethod test: no long implemented

--- test/core/server/Server.js
+++ test/core/server/Server.js
@@ -14,21 +14,3 @@ describe( 'Server', function(){
 		server.should.be.an.Object;
 	});
 });
-
-describe( 'Server.validateMethod', function(){
-	let server;
-	before( function(){
-		server = new Server();
-	});
-	it( 'should throw a TypeError with invalid method', function(){
-		server.validateMethod.bind( server, 'asd' ).should.throw( 'Not a valid method.' );
-	});
-	let validMethods = [ 'get', 'post', 'patch', 'put', 'delete', 'all' ];
-	for( let i = 0; i < validMethods.length; i++ ){
-		let method = validMethods[i];
-		it( 'should be fine with a valid ' + method + ' method', function(){
-			server.validateMethod.bind( server, method ).should.not.throw( 'Not a valid method.' );
-		});
-	}
-	
-});
