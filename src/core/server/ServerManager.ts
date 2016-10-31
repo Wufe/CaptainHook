8095fd32fea857a930ad506ef6fc019cfc7543a2
Wufe <wufe92@gmail.com>
2016-10-31 12:06:51 +0100

    Updated ServerManager to be able to setup authentication router

--- src/core/server/ServerManager.ts
+++ src/core/server/ServerManager.ts
@@ -1,17 +1,29 @@
-import Server from './Server';
+import * as Authentication from '../authentication';
 import GUIManager from '../gui/GUIManager';
+import Server from './Server';
 
 export default class ServerManager{
 
 	serverInstance: Server;
 	guiManager: GUIManager;
+	authenticationRouter: Authentication.Router;
 
 	constructor(){
 		this.serverInstance = new Server();
 	}
 
 	initialize(): void{
+		this.initializeAuthentication();
 		this.initializeGui();
+	}
+
+	initializeAuthentication(): void{
+		this.authenticationRouter = this.createAuthenticationRouter();
+		this.authenticationRouter.setup();
+	}
+
+	createAuthenticationRouter(): Authentication.Router{
+		return new Authentication.Router( this.serverInstance );
 	}
 
 	initializeGui(): void{
