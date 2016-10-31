f037c2315861dd417ec1928d9f6addd0d59d529c
Wufe <wufe92@gmail.com>
2016-10-31 12:06:09 +0100

    Authentication router

---
+++ src/core/authentication/Router.ts
@@ -0,0 +1,15 @@
+import Server from '../server/Server';
+
+export default class Router{
+
+	server: Server;
+
+	constructor( server: Server ){
+		this.server = server;
+	}
+
+	setup(): void{
+
+	}
+
+}
