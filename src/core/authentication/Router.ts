51d316284928ff079d16937dfb4cbf825205ce5c
Wufe <wufe92@gmail.com>
2016-10-31 14:17:33 +0100

    Sample authentication router class

--- src/core/authentication/Router.ts
+++ src/core/authentication/Router.ts
@@ -1,3 +1,7 @@
+/// <reference path="../../../typings/globals/express/index.d.ts" />
+/// <reference path="../../../typings/globals/express-serve-static-core/index.d.ts" />
+
+import {RequestHandler} from 'express';
 import Server from '../server/Server';
 
 export default class Router{
@@ -9,7 +13,17 @@ export default class Router{
 	}
 
 	setup(): void{
+		this.addAuthenticationRoutes();
+	}
 
+	addAuthenticationRoutes(): void{
+		this.server.express.post( '/auth', this.getAuthenticationRouteHandler() );
+	}
+
+	getAuthenticationRouteHandler(): RequestHandler{
+		return ( request, response, next ) => {
+			next();
+		};
 	}
 
 }
