1f1afd19338e1a7d24da88ec5e3ca0cc1a642217
Wufe <wufe92@gmail.com>
2016-11-01 18:59:08 +0100

    Fixing path identification in circleci with a workaround

--- src/core/chook/Environment.ts
+++ src/core/chook/Environment.ts
@@ -20,11 +20,14 @@ class Environment{
 
 	checkBuildDirectory(): void{
 		let isMocha: boolean = process.env.NODE_ENV == 'mocha';
+		let isCircleCI: boolean = process.env.NODE_ENV == 'circleci';
 		let args: string[] = process.argv;
 		let scriptPath: string = args[1];
 		let realPath = Fs.realpathSync( scriptPath );
 		if( isMocha ){
 			this.buildDirectory = Path.resolve( Path.join( Path.dirname( realPath ), '..', '..', '..', 'build' ) );
+		}else if( isCircleCI ){
+			this.buildDirectory = Path.resolve( Path.join( 'home', 'ubuntu', 'CaptainHook', 'build' ) );
 		}else{
 			this.buildDirectory = Path.resolve( Path.join( Path.dirname( realPath ), '..' ) );
 		}
