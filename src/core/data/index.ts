79dbb275834cde0c12cdab6c8bf791ec733ff19a
Wufe <wufe92@gmail.com>
2016-11-02 19:30:07 +0100

    Exposing accessinterface as it's a detached module

--- src/core/data/index.ts
+++ src/core/data/index.ts
@@ -1,7 +1,11 @@
+import Access from './Access';
+import {AccessInterface} from './Access';
 import Database from './Database';
 import * as Models from './models';
 
 export {
+	Access,
+	AccessInterface,
 	Database,
 	Models
 };
