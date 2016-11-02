2d6c0c2a1cb77455eb1c7973143c90effe9e2aab
Wufe <wufe92@gmail.com>
2016-11-02 15:25:02 +0100

    Added command under create:user argument for test purposes

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,6 +1,9 @@
 import Cli from './cli';
 import ServerManager from './server/ServerManager';
 
+import {User} from './actors';
+import {Encryption} from './authentication';
+
 export {
 	Cli,
 	ServerManager
@@ -14,4 +17,14 @@ if( args[ 2 ] === "start:server" ){
 	serverManager.initialize();
 	console.log( `Starting server..` );
 	serverManager.serverInstance.listen();
+}else if( args[ 2 ] === "create:user" ){
+	let encryption: Encryption = new Encryption( 'admin' );
+	let user: User = new User( 'admin', 'admin' );
+	user.save()
+		.then(( data ) => {
+			console.log( data );
+		})
+		.catch(( error: any ) => {
+			console.log( error );
+		})
 }
