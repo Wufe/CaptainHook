ce66afd1b7f4e6b9f7aa47d5f52fc2d701acac00
Wufe <wufe92@gmail.com>
2016-11-02 19:28:11 +0100

    Created data access module

---
+++ src/core/data/Access.ts
@@ -0,0 +1,83 @@
+/// <reference path="../../../typings/index.d.ts" />
+/// <reference path="../../../node_modules/@types/core-js/index.d.ts" />
+
+import * as Sequelize from 'sequelize';
+
+export interface AccessInterface<T>{
+	byId: ( identifier?: number | string, options?: Sequelize.FindOptions ) => Promise<T>;
+	one: ( options? : Sequelize.FindOptions ) => Promise<any>;
+	all: ( options? : Sequelize.FindOptions ) => Promise<any[]>;
+}
+
+export default class Access<T>{
+
+	actor: any;
+	model: Sequelize.Model<any, any>;
+
+	constructor( actor: any, model: Sequelize.Model<any, any> ){
+		this.actor = actor,
+		this.model = model;
+	}
+
+	getInterface(): AccessInterface<T>{
+		return {
+			byId: this.findById.bind( this ),
+			one: this.findOne.bind( this ),
+			all: this.findAll.bind( this )
+		};
+	}
+
+	createActor( data: any ): any{
+		let actorInstance: any = new this.actor();
+		for( let dataKey in data ){
+			let dataValue: any = data[ dataKey ];
+			actorInstance.set( dataKey, dataValue );
+		}
+		return actorInstance;
+	}
+
+	findById( identifier?: number | string, options?: Sequelize.FindOptions ) : Promise<T>{
+		return new Promise<T>( ( resolve, reject ) => {
+			this.model.findById( identifier, options )
+				.then( ( instance: any ) => {
+					if( instance ){
+						let dataValues: any = instance.dataValues;
+						if( !dataValues ){
+							reject( new Error( `No data returned.` ) );
+						}else{
+							let actorInstance: any = this.createActor( dataValues );
+							resolve( actorInstance );
+						}
+					}else{
+						reject( new Error( `No results found.` ) );
+					}
+				})
+				.catch( ( error: any ) => reject );
+		});
+		
+	}
+
+	findOne( options? : Sequelize.FindOptions ) : Promise<any>{
+		return new Promise<T>( ( resolve, reject ) => {
+			this.model.findOne( options )
+				.then( ( instance: any ) => {
+					if( instance ){
+						let dataValues: any = instance.dataValues;
+						if( !dataValues ){
+							reject( new Error( `No data returned.` ) );
+						}else{
+							let actorInstance: any = this.createActor( dataValues );
+							resolve( actorInstance );
+						}
+					}else{
+						reject( new Error( `No results found.` ) );
+					}
+				})
+				.catch( ( error: any ) => reject );
+		})
+	}
+
+	findAll( options? : Sequelize.FindOptions ) : Promise<any[]>{
+		return this.model.findAll( options );
+	}
+}
