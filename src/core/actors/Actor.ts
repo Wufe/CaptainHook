ebf3b81ba96691f9c63ed85928e2449650324d2d
Wufe <wufe92@gmail.com>
2016-11-02 15:43:22 +0100

    Added saving layer to Actor.create, in order to import insert result into the object itself.

--- src/core/actors/Actor.ts
+++ src/core/actors/Actor.ts
@@ -22,8 +22,24 @@ export default class Actor<T>{
 		this.data[ key ] = value;
 	}
 
-	save(): Promise<any>{
-		return this.model.create( this.data );
+	save(): Promise<T>{
+		return new Promise( ( resolve, reject ) => {
+			this.model.create( this.data )
+				.then( ( instance: any ) => {
+					let dataValues: any = instance.dataValues;
+					if( !dataValues ){
+						reject( new Error( `No data returned.` ) );
+					}else{
+						for( let dataKey in dataValues ){
+							let dataValue: any = dataValues[ dataKey ];
+							this.set( dataKey, dataValue );
+						}
+						resolve( this );
+					}
+				})
+				.catch( ( error: any ) => reject );
+		})
+		
 	}
 
 	static findById( identifier?: number | string, options?: Sequelize.FindOptions ) : Promise<any>{
