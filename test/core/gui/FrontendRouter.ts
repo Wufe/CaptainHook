d8222560d7abae58a46d1bc00a940d1d5f5a63cc
Wufe <wufe92@gmail.com>
2016-11-03 20:25:57 +0100

    Updated frontendrouter test with complex mock object

--- test/core/gui/FrontendRouter.ts
+++ test/core/gui/FrontendRouter.ts
@@ -3,15 +3,27 @@
 import * as Mocha from 'mocha';
 import * as Should from 'should';
 
-import {FrontendRouter} from '../../../src/core/gui';
+import {Environment} from '../../../src/core/chook';
+import {FrontendRouter, Renderer} from '../../../src/core/gui';
 import {Server} from '../../../src/core/server';
 
 describe( 'FrontendRouter', function(){
-	let frontendRouter: FrontendRouter;
-	before(function(){
-		let server = new Server();
-		frontendRouter = new FrontendRouter( server );
-	});
+	let server = new Server();
+	let frontendRouter: FrontendRouter = new FrontendRouter( server );
+	let mockExpressResponse: any = ( callback: ( ...values: any[] ) => void ) => {
+		return {
+			sendFile: ( resource: any ) => {
+				callback( resource );
+			},
+			status: ( code: number ) => {
+				return {
+					send: ( body: string ) => {
+						callback( code, body );
+					}
+				};
+			}
+		}
+	};
 	describe( 'getUrlPathname', function(){
 		let assetsMockRequest = {
 			url: 'https://test.dev/assets/javascript/test.chunk.js'
@@ -31,6 +43,35 @@ describe( 'FrontendRouter', function(){
 		});
 		it( 'should return false if the pathname is not the assets one', function(){
 			frontendRouter.isAssetsDir( '/test' ).should.be.exactly(false);
+		});
+	});
+	describe( 'getRenderer', function(){
+		it( `should return an object instance of Renderer`, () => {
+			Should( frontendRouter.getRenderer() ).be.instanceOf( Renderer );	
+		});
+	});
+	describe( 'getResourcePath', () => {
+		let resourcePath: string = `${Environment.buildDirectory}/resources/assets/css/main.css`;
+		it( `should return '${resourcePath}'`, () => {
+			frontendRouter.getResourcePath( 'css/main.css' ).should.be.equal( resourcePath );
+		});
+	});
+	describe( 'sendFile', () => {
+		it( `should call response's sendFile`, ( done ) => {
+			let resourcePath: string = `resource_.txt`;
+			frontendRouter.sendFile( mockExpressResponse(( resource: string ) => {
+				Should( resource ).be.equal( resourcePath );
+				done();
+			}), 'resource_.txt' );
+		});
+	});
+	describe( 'sendNotFound', () => {
+		it( `should call response's send function after status function`, ( done ) => {
+			frontendRouter.sendNotFound( mockExpressResponse(( code: number, body: string ) => {
+				Should( code ).be.equal( 404 );
+				Should( body ).be.equal( `Cannot find resource_.txt.` );
+				done();
+			}), 'resource_.txt' );
 		});
 	});
 
