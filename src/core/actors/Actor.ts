e8897ed9c6d69533b62dbe4d6023eb49f19b9af4
Wufe <wufe92@gmail.com>
2016-11-03 18:59:26 +0100

    Updated Actor refactoring the code in smaller -more testable- pieces

--- src/core/actors/Actor.ts
+++ src/core/actors/Actor.ts
@@ -25,20 +25,29 @@ export default class Actor<T>{
 		return new Promise( ( resolve, reject ) => {
 			this.model.create( this.data )
 				.then( ( instance: any ) => {
-					let dataValues: any = instance.dataValues;
-					if( !dataValues ){
-						reject( new Error( `No data returned.` ) );
-					}else{
-						for( let dataKey in dataValues ){
-							let dataValue: any = dataValues[ dataKey ];
-							this.set( dataKey, dataValue );
-						}
-						resolve( this );
-					}
+					this.handleSingleDataInstance( instance, resolve, reject );					
 				})
-				.catch( ( error: any ) => reject );
-		})
-		
+				.catch( ( error: any ) => {
+					reject( error );
+				});
+		});
+	}
+
+	private handleSingleDataInstance( instance: any, resolve: any, reject: any ): void{
+		let dataValues: any = instance.dataValues;
+		if( !dataValues ){
+			reject( new Error( `No data returned.` ) );
+		}else{
+			this.populateFromData( dataValues );
+			resolve( this );
+		}
+	}
+
+	private populateFromData( data: any ): void{
+		for( let dataKey in data ){
+			let dataValue: any = data[ dataKey ];
+			this.set( dataKey, dataValue );
+		}
 	}
 
 }
--- test/core/actors/Actor.ts
+++ test/core/actors/Actor.ts
@@ -10,18 +10,18 @@ import {Actors, ActorType} from '.';
 import {AccessInterface, Database} from '../../../src/core/data';
 
 describe( 'Actor', () => {
-	Actors.forEach( ( actor: ActorType ) => {
-		describe( actor.name, () => {
+	Actors.forEach( function( actor: ActorType ){
+		describe( actor.name, function(){
 			it( `should have a 'find' attribute`, () => {
 				let find: AccessInterface<any> = actor.class.find;
 				Should( find ).have.a.property( 'byId' );
 			});
-			describe( `created instanciating Actor with ${actor.name} data`, () => {
+			describe( `created instanciating Actor with ${actor.name} data`, function(){
 				let actorInstance = new Actor( actor.model, { 'test': 'valu_e' });
 				describe( `get`, () => {
 					it( `should return previously set value`, () => {
 						actorInstance.get( 'test' ).should.be.equal( 'valu_e' );
-					});	
+					});
 				});
 				describe( `set`, () => {
 					it( `should set a value and return it correctly`, () => {
@@ -29,6 +29,21 @@ describe( 'Actor', () => {
 						actorInstance.get( 'test' ).should.be.exactly( 'va_lue' );
 					});
 				});
+				if( actor.name == 'Test' ){
+					describe( `save`, function(){
+						it( `should have an id after successfull save`, ( done ) => {
+							actorInstance.set( 'test', 'value_' );
+							actorInstance.save()
+								.then( ( actorInstance: any ) => {
+									Should( typeof actorInstance.data.id ).be.equal( "number" );
+									done();
+								})
+								.catch( ( error: any ) => {
+									done( new Error( 'Should be able to save.' ) );
+								})
+						});
+					});
+				}
 			});
 		});
 	});
