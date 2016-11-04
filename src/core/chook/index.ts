f985e707b26d2267188f12144f2cfc3f5bca1e6b
Wufe <wufe92@gmail.com>
2016-11-05 00:51:21 +0100

    Adding Log class to chook module index

--- src/core/chook/index.ts
+++ src/core/chook/index.ts
@@ -1,9 +1,11 @@
 import EntryManager from './EntryManager';
 import Environment from './Environment';
+import Log from './Log';
 import * as Utils from './Utils';
 
 export {
 	EntryManager,
 	Environment,
+	Log,
 	Utils
 };
