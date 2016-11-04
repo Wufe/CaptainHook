f5cbd254c6142cf23cbb35e21e0b21c5f3ca6c6d
Wufe <wufe92@gmail.com>
2016-11-04 18:56:16 +0100

    Delegated auth check to Authentication class

---
+++ src/core/auth/Authentication.ts
@@ -0,0 +1,38 @@
+import {Credentials, Encryption} from '.';
+import {User} from '../actors';
+
+export default class Authentication{
+
+	credentials: Credentials;
+	
+	constructor( credentials?: Credentials ){
+		this.credentials = credentials;
+	}
+
+	validateCredentials(): Promise<User>{
+		return new Promise<User>( ( resolve, reject ) => {
+			User.find.one({
+				where: {
+					username: this.credentials.username
+				}
+			})
+			.then( ( user: User ) => {
+				let isPasswordValid: boolean = this.validatePassword( user.get( 'password' ), this.credentials.password );
+				if( !isPasswordValid ){
+					reject();
+				}else{
+					resolve( user );
+				}
+			})
+			.catch( ( error: any ) => {
+				reject( error );
+			});
+		});
+	}
+
+	validatePassword( hashedPassword: string, plainPassword: string ): boolean{
+		let encryption: Encryption = new Encryption( plainPassword );
+		return encryption.compare( hashedPassword );
+	}
+
+}
