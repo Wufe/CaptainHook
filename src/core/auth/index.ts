f0ecba935c488faa9603c7f1d8ce715ced176d32
Wufe <wufe92@gmail.com>
2016-11-04 18:56:41 +0100

    Exposing Authentication to auth module index

--- src/core/auth/index.ts
+++ src/core/auth/index.ts
@@ -1,7 +1,11 @@
+import Authentication from './Authentication';
 import Encryption from './Encryption';
+import {Credentials} from './Router';
 import Router from './Router';
 
 export {
+	Authentication,
+	Credentials,
 	Encryption,
-	Router
+	Router	
 };
