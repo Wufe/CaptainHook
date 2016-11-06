b492e59b59aba5b04cf500f5d266fb74d5f4cd38
Wufe <wufe92@gmail.com>
2016-11-06 12:42:49 +0100

    Using a argumentparser creator to demand the creation and parsing of all subcommands

---
+++ src/core/cli/Creator.ts
@@ -0,0 +1,14 @@
+/// <reference path="../../../typings/index.d.ts" />
+
+import * as ArgParse from 'argparse';
+import {ArgumentParser} from 'argparse';
+
+export default class Creator{
+
+	constructor(){}
+
+	createArgumentParser(): ArgumentParser{
+		return null;
+	}
+
+}
