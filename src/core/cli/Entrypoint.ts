7ab9ac05f34eebcb732da4419132fe2ae8d4ba32
Wufe <wufe92@gmail.com>
2016-11-06 12:38:01 +0100

    Root argument parser defined

--- src/core/cli/Entrypoint.ts
+++ src/core/cli/Entrypoint.ts
@@ -3,13 +3,19 @@
 import * as ArgParse from 'argparse';
 import {ArgumentParser} from 'argparse';
 
+import {Environment} from '../chook';
+
 export default class Entrypoint{
 
 	argumentParser: ArgumentParser;
 	constructor(){}
 
 	createArgumentParser(): void{
-		let argumentParser: ArgumentParser
+		let argumentParser: ArgumentParser = new ArgumentParser({
+			version: Environment.package.version,
+			description: Environment.package.description,
+			addHelp: true
+		});
 	}
 
 }
