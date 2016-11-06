1f72e927b1c000842c30ca7d414117c79974b70c
Wufe <wufe92@gmail.com>
2016-11-06 12:17:26 +0100

    Importing ArgumentParser type

--- src/core/cli/Entrypoint.ts
+++ src/core/cli/Entrypoint.ts
@@ -1,15 +1,15 @@
 /// <reference path="../../../typings/index.d.ts" />
 
 import * as ArgParse from 'argparse';
+import {ArgumentParser} from 'argparse';
 
 export default class Entrypoint{
 
-	argumentParser: ArgParse.ArgumentParser;
-
+	argumentParser: ArgumentParser;
 	constructor(){}
 
 	createArgumentParser(): void{
-		
+		let argumentParser: ArgumentParser
 	}
 
 }
