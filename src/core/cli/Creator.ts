3d69c2aca83f636b91db1bf1dcbb8bcb35e8d348
Wufe <wufe92@gmail.com>
2016-11-06 14:33:01 +0100

    Implemented parser creation

--- src/core/cli/Creator.ts
+++ src/core/cli/Creator.ts
@@ -1,9 +1,11 @@
 /// <reference path="../../../typings/index.d.ts" />
 
 import * as ArgParse from 'argparse';
-import {ArgumentParser} from 'argparse';
+import {ArgumentParser, SubParser, SubparserOptions} from 'argparse';
 
 import {Environment} from '../chook';
+
+const commands = require( './commands.json' );
 
 export default class Creator{
 
@@ -15,7 +17,48 @@ export default class Creator{
 			description: Environment.package.description,
 			addHelp: true
 		});
+		let rootProperties: any = commands;
+		this.addSubparser( argumentParser, rootProperties );
 		return argumentParser;
+	}
+
+	addSubparser( rootParser: ArgumentParser, properties: any ): void{
+		let {title, dest, required, commands}: {
+			title?: string;
+			dest?: string;
+			required?: boolean;
+			commands?: any[];
+		} = properties;
+		let subparser: SubParser = rootParser.addSubparsers({
+			title,
+			dest
+		});
+		commands.forEach( ( command: any ) => {
+			let {id, args, sub}: {
+				id?: any;
+				args?: any[];
+				sub?: any;
+			} = command;
+			if( command.args )
+				delete command.args;
+			if( command.sub )
+				delete command.sub;
+			if( command.id )
+				delete command.id;
+			let parser: ArgumentParser = subparser.addParser( id, command );
+			if( args ){
+				args.forEach( ( arg: any ) => {
+					let {id}: {
+						id?: any;
+					} = arg;
+					if( arg.id )
+						delete arg.id;
+					parser.addArgument( id, arg );
+				});
+			}
+			if( sub )
+				this.addSubparser( parser, sub );
+		});
 	}
 
 }
