9fdf63c0e88eb914cfe51d3de4bc1521033b9bc5
Wufe <wufe92@gmail.com>
2016-11-06 12:46:58 +0100

    Added root argument parser creation

--- src/core/cli/Creator.ts
+++ src/core/cli/Creator.ts
@@ -3,12 +3,19 @@
 import * as ArgParse from 'argparse';
 import {ArgumentParser} from 'argparse';
 
+import {Environment} from '../chook';
+
 export default class Creator{
 
 	constructor(){}
 
 	createArgumentParser(): ArgumentParser{
-		return null;
+		let argumentParser: ArgumentParser = new ArgumentParser({
+			version: Environment.package.version,
+			description: Environment.package.description,
+			addHelp: true
+		});
+		return argumentParser;
 	}
 
 }
