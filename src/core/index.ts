4d333bdfb1568a86de72061105274918857b9b0a
Wufe <wufe92@gmail.com>
2016-11-07 13:26:53 +0100

    Removing sample code

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,30 +1,7 @@
 import {Entrypoint} from './cli';
 import {ServerManager} from './net';
 
-import {Actor, Hook, User, Test} from './actors';
-import {Encryption} from './auth';
-
 export {
 	Entrypoint,
 	ServerManager
 };
-
-/* Sample code */
-
-let args: string[] = process.argv;
-if( args[ 2 ] === "start:server" ){
-	let serverManager: ServerManager = new ServerManager();
-	serverManager.initialize();
-	console.log( `Starting server..` );
-	serverManager.serverInstance.listen();
-}else if( args[ 2 ] === "create:user" ){
-	let user: User = new User( 'asd', 'admin' );
-	user.save()
-		.then( data => console.log( data ) )
-		.catch( error => console.log( error ) );
-}else if( args[ 2 ] === "create:test" ){
-	let test: Test = new Test( 'test' );
-	test.save()
-		.then( data => console.log( data ) )
-		.catch( error => console.log( error ) );
-}
