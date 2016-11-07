09cb5c2239e8905d48e43e8cc447fd2c252219a6
Wufe <wufe92@gmail.com>
2016-11-07 13:37:32 +0100

    Updated Configuration's test

--- test/core/Configuration.ts
+++ test/core/Configuration.ts
@@ -5,50 +5,45 @@ import * as Should from 'should';
 
 import Configuration from '../../src/core/Configuration';
 
-describe( 'Configuration', function(){
-	describe( 'Configuration\'s instance', function(){
-		it( 'should be an object', function(){
+describe( 'Configuration', () => {
+	describe( `its instance`, () => {
+		it( 'should be an object', () => {
 			Configuration.should.be.an.Object;
 		});
 	});
-});
-
-describe( 'Configuration.readConfiguration', function(){
-	it( 'should produce a default configuration object', function(){
-		Configuration.readConfiguration();
-		let typeOfConfigurationObject: string = typeof Configuration.configuration;
-		typeOfConfigurationObject.should.not.be.equal( "undefined" );
+	describe( `readConfiguration`, () => {
+		it( 'should produce a default configuration object', () => {
+			Configuration.readConfiguration();
+			let typeOfConfigurationObject: string = typeof Configuration.configuration;
+			typeOfConfigurationObject.should.not.be.equal( "undefined" );
+		});
 	});
-});
-
-describe( 'Configuration.set', function(){
-	it( 'should add a string configuration value', function(){
-		Configuration.set( 'lol', 'asd' );
-		Configuration.should.have.propertyByPath( 'configuration', 'asd' ).which.is.equal( "lol" );
+	describe( `set`, () => {
+		it( 'should add a string configuration value', () => {
+			Configuration.set( 'lol', 'asd' );
+			Configuration.should.have.propertyByPath( 'configuration', 'asd' ).which.is.equal( "lol" );
+		});
+		it( 'should add a array configuration value', () => {
+			Configuration.set( [ 'lol', 'rofl' ], 'asd' );
+			Configuration.should.have.propertyByPath( 'configuration', 'asd' ).which.is.Array;
+			Configuration.should.have.propertyByPath( 'configuration', 'asd' ).which.containDeep([ "lol", "rofl" ]);
+		});
+		it( 'should add a object configuration value', () => {
+			Configuration.set( { lol: 'lol', rofl: 'rofl' }, 'asd' );
+			Configuration.should.have.propertyByPath( 'configuration', 'asd', 'lol' ).which.equal( 'lol' );
+			Configuration.should.have.propertyByPath( 'configuration', 'asd', 'rofl' ).which.equal( 'rofl' );
+		});
 	});
-	it( 'should add a array configuration value', function(){
-		Configuration.set( [ 'lol', 'rofl' ], 'asd' );
-		Configuration.should.have.propertyByPath( 'configuration', 'asd' ).which.is.Array;
-		Configuration.should.have.propertyByPath( 'configuration', 'asd' ).which.containDeep([ "lol", "rofl" ]);
-	});
-	it( 'should add a object configuration value', function(){
-		Configuration.set( { lol: 'lol', rofl: 'rofl' }, 'asd' );
-		Configuration.should.have.propertyByPath( 'configuration', 'asd', 'lol' ).which.equal( 'lol' );
-		Configuration.should.have.propertyByPath( 'configuration', 'asd', 'rofl' ).which.equal( 'rofl' );
-	});
-});
-
-describe( 'Configuration.get', function(){
-	before( function(){
+	describe( `get`, () => {
 		Configuration.set( 'dsa', 'asd' );
 		Configuration.set({ lal: 'rofl' }, 'lol' );
-	});
-	it( 'should get a single value', function(){
-		let value = Configuration.get( 'asd' );
-		value.should.equal( "dsa" );
-	})
-	it( 'should get nested element value', function(){
-		let value = Configuration.get( 'lol', 'lal' );
-		value.should.equal( "rofl" );
+		it( 'should get a single value', () => {
+			let value = Configuration.get( 'asd' );
+			value.should.equal( "dsa" );
+		})
+		it( 'should get nested element value', () => {
+			let value = Configuration.get( 'lol', 'lal' );
+			value.should.equal( "rofl" );
+		});
 	});
 });
