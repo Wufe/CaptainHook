7d63db4775ce9bd1a5e0d4ba422e17911499da14
Wufe <wufe92@gmail.com>
2016-11-07 11:33:21 +0100

    Updated required modules

--- src/core/chook/Log.ts
+++ src/core/chook/Log.ts
@@ -8,13 +8,14 @@ import * as Moment from 'moment';
 
 import * as Fs from 'fs';
 import * as Path from 'path';
-import * as Winston from 'winston';
+import {Logger, LoggerInstance, transports, TransportInstance} from 'winston';
 
+const {Console, File} = transports;
 const logDirectory = Path.join( Environment.buildDirectory, 'resources', 'log' );
 
 export class Log{
 
-	logger: Winston.LoggerInstance;
+	logger: LoggerInstance;
 	directoryFound: boolean = false;
 
 	constructor(){}
@@ -38,24 +39,24 @@ export class Log{
 	}
 
 	createLogger(): void{
-		let logger: Winston.LoggerInstance = new Winston.Logger({
+		let logger: LoggerInstance = new Logger({
 			transports: this.getTransports()
 		});
 		this.logger = logger;
 	}
 
-	getTransports(): Winston.TransportInstance[]{
-		let transports: Winston.TransportInstance[] = [];
+	getTransports(): TransportInstance[]{
+		let transports: TransportInstance[] = [];
 		if( this.directoryFound ){
 			transports.push(
-				new Winston.transports.File({
+				new File({
 					filename: Path.join( logDirectory, 'generic.log' )
 				})
 			);
 		}
 		if( !this.isQuiet() ){
 			transports.push(
-				new Winston.transports.Console({
+				new Console({
 					level: 'debug',
 					prettyPrint: true,
 					colorize: true,
