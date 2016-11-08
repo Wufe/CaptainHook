c139e629579c3c3f8143b2c675bdf911aee23ff0
Wufe <wufe92@gmail.com>
2016-11-08 16:01:41 +0100

    Logging hostname and port

--- src/core/net/Server.ts
+++ src/core/net/Server.ts
@@ -1,4 +1,5 @@
 import Configuration from '../Configuration';
+import {Log} from '../chook';
 import * as BodyParser from 'body-parser';
 import * as Express from 'express';
 import * as Http from 'http';
@@ -18,6 +19,7 @@ export default class Server{
 			hostname: string;
 			port: number;
 		} = Configuration.get( 'server' );
+		Log( 'info', `[${server.hostname}:${server.port}] Starting server..` );
 		this.express.listen( server.port, server.hostname ).on( 'error', console.log );
 	}
 
