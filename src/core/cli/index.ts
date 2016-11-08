7e69ba2dd11d216183194d80920ddac550f35f26
Wufe <wufe92@gmail.com>
2016-11-08 13:09:33 +0100

    Added Dispatcher and its main function to the module index

--- src/core/cli/index.ts
+++ src/core/cli/index.ts
@@ -1,7 +1,10 @@
 import Creator from './Creator';
+import {Dispatcher, dispatch} from './Dispatcher';
 import Entrypoint from './Entrypoint';
 
 export {
 	Creator,
+	Dispatcher,
+	dispatch,
 	Entrypoint
 };
