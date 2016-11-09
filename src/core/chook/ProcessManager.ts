5a3c6886a564ad6b051eef1d7199d62c659df2b0
Wufe <wufe92@gmail.com>
2016-11-09 20:45:25 +0100

    Created ProcessManager class.
    It exposes a Process interface that is responsible of holding
    process information.
    
    The ProcessManager class needs a Process object and handles
    creation of detached processes, saving or deletion of their information,
    kill and loading.
    
    The loadProcess static function returns an instance of ProcessManager
    with loaded -from file- information about the process itself.
    
    The class needs to be tested.

--- src/core/chook/ProcessManager.ts
+++ src/core/chook/ProcessManager.ts
@@ -1,3 +1,97 @@
-export default class ProcessManager{
-	
+/// <reference path="../../../typings/index.d.ts" />
+
+import {Environment, Log, Utils} from '.';
+
+import * as ChildProcess from 'child_process';
+import * as Fs from 'fs';
+import * as Moment from 'moment';
+import * as Path from 'path';
+import * as Yaml from 'js-yaml';
+
+export interface Process{
+	binary: string;
+	arguments?: string[];
+	cwd?: string;
+	pid?: number;
+	startedAt?: string;
+}
+
+export class ProcessManager{
+
+	processInfo: Process;
+	defaultArguments = [];
+
+	constructor( processInfo: Process ){
+		if( !processInfo.arguments )
+			processInfo.arguments = this.defaultArguments;
+		if( !processInfo.cwd )
+			processInfo.cwd = Path.dirname( processInfo.binary );
+		this.processInfo = processInfo;
+	}
+
+	spawnProcess(): void{
+		let spawned: ChildProcess.ChildProcess;
+		try{
+			spawned = ChildProcess.spawn( this.processInfo.binary, this.processInfo.arguments, {
+				cwd: this.processInfo.cwd,
+				detached: true,
+				stdio: 'ignore'
+			});
+			spawned.unref();
+			this.processInfo.pid = spawned.pid;
+			this.processInfo.startedAt = ( Moment().format() );
+		}catch( error ){
+			Log( 'error', `Cannot start the process.`, this.processInfo, error );
+		}
+	}
+
+	saveProcess( filename: string = 'process.yml' ){
+		let filepath: string = Path.join( Environment.buildDirectory, 'resources', filename );
+		try{
+			let yamlProcess: string = Yaml.safeDump( this.processInfo );
+			Fs.writeFileSync( filepath, yamlProcess )
+		}catch( error ){
+			Log( 'error', `Cannot write process information to file [${filename}].`, this.processInfo, error );
+		}
+	}
+
+	deleteProcess( filename: string = 'process.yml' ){
+		let filepath: string = Path.join( Environment.buildDirectory, 'resources', filename );
+		if( !Utils.isFile( filepath ) ){
+			Log( 'warn', `Process information file does not exist [${filename}].` );
+			return;
+		}
+		try{
+			Fs.unlinkSync( filepath );
+		}catch( error ){
+			Log( 'error', `Cannot delete process information file [${filename}].`, error );
+		}
+	}
+
+	killProcess( signal: string|number = 'SIGINT' ): boolean{
+		if( !this.processInfo ){
+			Log( 'error', `No PID available. Cannot kill the process.` );
+			return false;
+		}
+		try{
+			process.kill( this.processInfo.pid, signal );
+			return true;
+		}catch( error ){
+			Log( 'error', `Cannot kill process.`, error );
+			return false;
+		}
+	}
+
+	static loadProcess( filename: string = 'process.yml' ): ProcessManager{
+		let filepath: string = Path.join( Environment.buildDirectory, 'resources', filename );
+		try{
+			let fileContent: string = Fs.readFileSync( filepath, 'utf8' );
+			let processInfo: Process = Yaml.safeLoad( fileContent );
+			return new ProcessManager( processInfo );
+		}catch( error ){
+			Log( 'error', `Cannot load process from file [${filename}].`, error );
+			return;
+		}
+	}
+
 }
