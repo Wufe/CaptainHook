53be5d594e4cd79ce1855e6699a53aacec8a422e
Wufe <wufe92@gmail.com>
2016-11-09 23:43:40 +0100

    Exposing Process and ProcessManager to module index

--- src/core/chook/index.ts
+++ src/core/chook/index.ts
@@ -1,11 +1,14 @@
 import EntryManager from './EntryManager';
 import Environment from './Environment';
 import Log from './Log';
+import {Process, ProcessManager} from './ProcessManager';
 import * as Utils from './Utils';
 
 export {
 	EntryManager,
 	Environment,
 	Log,
+	Process,
+	ProcessManager,
 	Utils
 };
