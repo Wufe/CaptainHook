9cbe90924392c003cb186041bdd5ecc836419b51
Simone Bembi <wufe92@gmail.com>
2016-12-21 02:24:00 +0100

    Added cookie parsing for auth and better error log on express listen

--- src/core/net/Server.ts
+++ src/core/net/Server.ts
@@ -1,7 +1,9 @@
 import {Configuration, Log} from '../chook';
 import * as BodyParser from 'body-parser';
+import * as CookieParser from 'cookie-parser';
 import * as Express from 'express';
 import * as Http from 'http';
+import {red} from 'chalk';
 
 export default class Server{
 
@@ -11,6 +13,7 @@ export default class Server{
 		this.express = Express();
 		this.express.use( BodyParser.urlencoded({ extended: false }) );
 		this.express.use( BodyParser.json() );
+		this.express.use( CookieParser() );
 	}
 
 	listen(): void{
@@ -19,7 +22,11 @@ export default class Server{
 			port: number;
 		} = Configuration.get( 'server' );
 		Log( 'info', `[${server.hostname}:${server.port}] Starting server..` );
-		this.express.listen( server.port, server.hostname ).on( 'error', console.log );
+		this.express.listen( server.port, server.hostname ).on( 'error', ( error: any ) => {
+			Log( "error", red( error.message ), error );
+		});	
+			
+		
 	}
 
 }
